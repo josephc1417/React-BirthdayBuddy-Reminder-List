9c1371bf1bef231f908e6e1cfb857e092ac5ea68
nova vita <42131370+josephc1417@users.noreply.github.com>
2023-09-05 03:44:32 -0400

    Simple list using useState

---
+++ src/components/person.jsx
@@ -0,0 +1,13 @@
+
+
+export const Person = ({name,age,image}) => {
+  return (
+    <article className="person">
+        <img src={image} alt= {name} className="img"/>
+        <div>
+            <h4>{name}</h4>
+            <p>{age} Years Old</p>
+        </div>
+    </article>
+  )
+}
---
+++ src/data.js
@@ -0,0 +1,27 @@
+// we can call this obj anything that we want when we are importing it.
+export default [
+    {
+        id:1,
+        name:'Ashley',
+        age:32,
+        image:'https://media.gq.com/photos/55828f78e52bc4b477a98e17/1:1/w_628,h_628,c_limit/blogs-the-feed-tinashe-selfie-628.jpg'
+    },
+    {
+        id:2,
+        name:'bryan',
+        age:22,
+        image:'https://i.pinimg.com/736x/d4/c9/07/d4c907274eebe6c4595eedf179951693.jpg'
+    },
+    {
+        id:3,
+        name:'Lisa',
+        age:36,
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK0wNHozyQ3sYhNQ6B4VL4GgncaNrdTC34rw&usqp=CAU'
+    },
+    {
+        id:4,
+        name:'tim',
+        age:23,
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMorHbpdlFwOnVJ1sRogy_n1hkFKEZEWDX7mL5DuOZ7o7-pAlARrv2gRGQj6_aKLGHXKI&usqp=CAU'
+    }
+]
